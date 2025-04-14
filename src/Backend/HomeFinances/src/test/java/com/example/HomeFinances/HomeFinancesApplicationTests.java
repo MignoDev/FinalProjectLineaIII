39bc2450e0ec831d9bc6a4baa9f84f44d8edbc3a
MignoDev <110192825+MignoDev@users.noreply.github.com>
2025-04-14 12:26:09 -0500

    Creación de proyecto backend

---
+++ src/Backend/HomeFinances/src/test/java/com/example/HomeFinances/HomeFinancesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HomeFinances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeFinancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
