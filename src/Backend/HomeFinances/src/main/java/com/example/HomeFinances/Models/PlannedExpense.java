fc454e108a5f3cc1926f9b4fd00526aad2abcf22
MignoDev <110192825+MignoDev@users.noreply.github.com>
2025-04-14 19:56:46 -0500

    creacion de modelos, servicios y repositorios

---
+++ src/Backend/HomeFinances/src/main/java/com/example/HomeFinances/Models/PlannedExpense.java
@@ -0,0 +1,4 @@
+package com.example.HomeFinances.Models;
+
+public class PlanedExpense {
+}
---
+++ src/Backend/HomeFinances/src/main/java/com/example/HomeFinances/Repositories/ActualExpenseRepository.java
@@ -0,0 +1,4 @@
+package com.example.HomeFinances.Repositories;
+
+public interface ActualExpenseRepository {
+}
