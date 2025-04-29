93375b489d24bad96e4fa2ecaa7e454567daaea1
MignoDev <110192825+MignoDev@users.noreply.github.com>
2025-04-29 14:03:46 -0500

    Mejora de visual para probar conexion con api

--- src/Frontend/HomeFinance/app/src/main/java/com/example/homefinance/Screen/UserScreen.kt
+++ src/Frontend/HomeFinance/app/src/main/java/com/example/homefinance/Screen/UserScreen.kt
@@ -7,9 +7,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
@@ -36,7 +39,7 @@ import kotlinx.coroutines.launch
 @Composable
 fun UserScreen(viewModel: UserViewModel = viewModel()) {
     val users by viewModel.user.observeAsState(emptyList())
-    var idBusqueda by remember { mutableStateOf("") }
+    var searchValue by remember { mutableStateOf("") }
     var userName by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var nickName by remember { mutableStateOf("") }
@@ -48,6 +51,9 @@ fun UserScreen(viewModel: UserViewModel = viewModel()) {
     var showDialog by remember { mutableStateOf(false) }
     var usuarioAEliminar by remember { mutableStateOf<User?>(null) }
 
+    //Estado para controlar la muestra de errores
+    var formSubmitted by remember { mutableStateOf(false)}
+
     val scope = rememberCoroutineScope()
     val context = LocalContext.current
 
@@ -56,15 +62,16 @@ fun UserScreen(viewModel: UserViewModel = viewModel()) {
     }
 
     val usuariosFiltrados = users?.filter {
-        it.id.toString().contains(idBusqueda, ignoreCase = true)
+        it.userName.toString().contains(searchValue, ignoreCase = true) ||
+        it.nickName.toString().contains(searchValue, ignoreCase = true)
     } ?: emptyList()
 
     Column (modifier = Modifier.padding(top = 60.dp)) {
         Text(text = "Lista de usuarios", style = MaterialTheme.typography.headlineSmall)
 
         TextField(
-            value = idBusqueda,
-            onValueChange = {idBusqueda = it},
+            value = searchValue,
+            onValueChange = {searchValue = it},
             label = { Text("Buscar por código") },
             leadingIcon = {
                 Icon(Icons.Filled.Search, contentDescription = "Codigo Icon")
@@ -82,14 +89,15 @@ fun UserScreen(viewModel: UserViewModel = viewModel()) {
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = 8.dp),
-            isError = userName.isBlank()
+            isError = (formSubmitted && userName.isBlank())
         )
 
-        if (userName.isBlank()) {
+        if (formSubmitted && userName.isBlank()) {
             Text(
                 text = "El nombre de usuario es obligatorio",
                 color = MaterialTheme.colorScheme.error,
-                modifier = Modifier.padding(top = 8.dp)
+                modifier = Modifier.padding(top = 2.dp),
+                style = MaterialTheme.typography.labelSmall
             )
         }
 
@@ -97,7 +105,7 @@ fun UserScreen(viewModel: UserViewModel = viewModel()) {
             value = password,
             onValueChange = {password = it},
 
-            label = { Text("nombre de usuario") },
+            label = { Text("Contraseña") },
 
             visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
@@ -110,8 +118,17 @@ fun UserScreen(viewModel: UserViewModel = viewModel()) {
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = 8.dp),
-            isError = userName.isBlank()
+            isError = ( formSubmitted && password.isBlank())
         )
+
+        if (formSubmitted && password.isBlank()) {
+            Text(
+                text = "El nombre de usuario es obligatorio",
+                color = MaterialTheme.colorScheme.error,
+                modifier = Modifier.padding(top = 2.dp),
+                style = MaterialTheme.typography.labelSmall
+            )
+        }
 
         TextField(
             value = nickName,
@@ -128,14 +145,18 @@ fun UserScreen(viewModel: UserViewModel = viewModel()) {
             onClick = {
                 if (userName.isNotEmpty() && password.isNotEmpty()) {
                     scope.launch {
-                        val nuevoUsuario = UserRequest(userName = userName, password = password, nickName = if(nickName.isEmpty()) userName else nickName)
-                        viewModel.createUser(nuevoUsuario)
-                        userName = ""
-                        password = ""
-                        nickName = ""
-                        // Mostrar el Toast de éxito
-                        Toast.makeText(context, "Usuario insertado", Toast.LENGTH_SHORT).show()
-                        viewModel.listUsers()
+                        formSubmitted = true
+                        if (userName.isNotEmpty() || password.isNotEmpty())
+                        {
+                            val nuevoUsuario = UserRequest(userName = userName, password = password, nickName = if(nickName.isEmpty()) userName else nickName)
+                            viewModel.createUser(nuevoUsuario)
+                            userName = ""
+                            password = ""
+                            nickName = ""
+                            // Mostrar el Toast de éxito
+                            Toast.makeText(context, "Usuario insertado", Toast.LENGTH_SHORT).show()
+                            viewModel.listUsers()
+                        }
                     }
                 }
             },
@@ -147,6 +168,85 @@ fun UserScreen(viewModel: UserViewModel = viewModel()) {
             Spacer(modifier = Modifier.width(8.dp))
             Text(text = "Agregar Estudiante")
         }
+
+        if (usuariosFiltrados.isEmpty()) {
+            Text(text = "No hay estudiantes disponibles.")
+        } else {
+            LazyColumn(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                items(usuariosFiltrados) { usuario ->
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp),
+                        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+                    ) {
+                        Column(modifier = Modifier.padding(16.dp)) {
+                            Text(
+                                text = "Usuario: ${usuario.userName}",
+                                style = MaterialTheme.typography.titleMedium
+                            )
+
+                            Text(
+                                text = "Alias: ${usuario.nickName}",
+                                style = MaterialTheme.typography.bodyMedium
+                            )
+
+                            // Botón de Eliminar Estudiante con ícono
+                            Button(
+                                onClick = {
+                                    // Mostrar el diálogo de confirmación de eliminación
+                                    usuarioAEliminar = usuario
+                                    showDialog = true
+                                },
+                                modifier = Modifier.padding(top = 8.dp),
+                                contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+                            ) {
+                                Icon(
+                                    Icons.Filled.Delete,
+                                    contentDescription = "Eliminar Estudiante Icon"
+                                )
+                                Spacer(modifier = Modifier.width(8.dp))
+                                Text(text = "Eliminar")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            title = { Text("Confirmación") },
+            text = { Text("¿Está seguro que desea eliminar a este usuario?") },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        // Eliminar el estudiante si el usuario confirma
+                        usuarioAEliminar?.let {
+                            viewModel.deleteUser(it.id)
+                            // Mostrar el Toast de éxito
+                            Toast.makeText(context, "Usuario eliminado", Toast.LENGTH_SHORT).show()
+                            viewModel.listUsers()
+                        }
+                        showDialog = false
+                    }
+                ) {
+                    Text("Sí")
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = { showDialog = false }
+                ) {
+                    Text("No")
+                }
+            }
+        )
     }
 
 
