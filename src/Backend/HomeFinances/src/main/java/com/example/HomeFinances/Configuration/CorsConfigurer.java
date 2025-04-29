ef9728a4a9e8efd8b9b02e4dccef2eeec3edb41e
MignoDev <110192825+MignoDev@users.noreply.github.com>
2025-04-28 20:09:11 -0500

    inclusion de configuraicon de cors y creacion de proyecto front end

---
+++ src/Backend/HomeFinances/src/main/java/com/example/HomeFinances/Configuration/CorsConfigurer.java
@@ -0,0 +1,25 @@
+package com.example.HomeFinances.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer ()
+    {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/*")
+                        .allowedOrigins("10.0.2.2:8080")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
