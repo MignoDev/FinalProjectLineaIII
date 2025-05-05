9d62c56b63f59d653571755f3ef8d4db3d18bd41
MignoDev <110192825+MignoDev@users.noreply.github.com>
2025-05-05 01:26:58 -0500

    Creacion de navegacion para home

--- src/Frontend/HomeFinance/app/src/main/java/com/example/homefinance/MainActivity.kt
+++ src/Frontend/HomeFinance/app/src/main/java/com/example/homefinance/MainActivity.kt
@@ -1,35 +1,11 @@
 package com.example.homefinance
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.LineHeightStyle
-import androidx.navigation.NavHost
-import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.homefinance.Core.Navigation.AppNavGraph
-import com.example.homefinance.Core.Navigation.BottomNavBar
-import com.example.homefinance.Core.Navigation.NavItemList
-import com.example.homefinance.Screen.BottomNavScreen
-import com.example.homefinance.Screen.HomeScreen
-import com.example.homefinance.Screen.InvestmentScreen
-import com.example.homefinance.Screen.MainScreen
-import com.example.homefinance.Screen.MovementScreen
-import com.example.homefinance.Screen.ProfileScreen
-import com.example.homefinance.Screen.ResumeScreen
-import com.example.homefinance.Screen.UserScreen
 import com.example.homefinance.ui.theme.HomeFinanceTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,7 +15,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             val navController = rememberNavController()
             HomeFinanceTheme {
-                MainScreen(navController.navigate(settings))
+                AppNavGraph()
             }
         }
     }
--- src/Frontend/HomeFinance/app/src/main/java/com/example/homefinance/Screen/MainScreen.kt
+++ src/Frontend/HomeFinance/app/src/main/java/com/example/homefinance/Screen/MainScreen.kt
@@ -2,11 +2,17 @@ package com.example.homefinance.Screen
 
 import android.annotation.SuppressLint
 import android.graphics.drawable.Icon
+import android.provider.CalendarContract
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.*
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
@@ -38,17 +44,7 @@ fun MainScreen(navigateToSettings: () -> Unit) {
             )
         }
     ) {
-        Column (modifier = Modifier.fillMaxSize()) {
-            Row (modifier = Modifier.align(Alignment.End)) {
-                IconButton(
-                    onClick = navigateToSettings,
-                ) {
-                    Icons.Filled.Settings
-                }
-            }
-            ContentScreen(selectedIndex)
-        }
-
+        ContentScreen(selectedIndex)
     }
 }
 
